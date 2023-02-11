f8938df2322d5893127755c9b6b1005dccc14f1e
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-02-11 10:09:27 +0100

    Do things better, faster, stronger

---
+++ README.md
@@ -0,0 +1,8 @@
+# Eslint setup
+
+```
+npm install --save-dev eslint eslint-config-universe eslint-plugin-auto-import eslint-plugin-react eslint-plugin-react-native prettier
+
+```
+
+
--- src/components/ShoppingListInput.js
+++ src/components/ShoppingListInput.js
@@ -6,14 +6,24 @@ const ShoppingListInput = ({ visible, onCancel }) => {
     <Modal animationType="slide" visible={visible}>
       <View>
         <Text>What to buy?</Text>
-        <TouchableOpacity
-          style={styles.buttonContainer}
-          onPress={() => {
-            onCancel();
-          }}
-        >
-          <Text style={styles.buttonText}>Close</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              onCancel();
+            }}
+          >
+            <Text style={styles.buttonText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              console.log("adding item..");
+            }}
+          >
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Modal>
   );
@@ -32,6 +42,20 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     width: "100%",
+  },
+  button: {
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    margin: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
   },
 });
 
